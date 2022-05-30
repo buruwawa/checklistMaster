092ed0b4a15985fbd0b9736134dcdaaf56ec34e3
buruwawa <buruwawa@gmail.com>
2022-05-30 12:47:16 +0300

    first commit-successfully

--- resources/views/livewire/checklist.blade.php
+++ resources/views/livewire/checklist.blade.php
@@ -14,8 +14,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <link href="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/css/select2.min.css" rel="stylesheet" />
 <script src="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/js/select2.min.js"></script>
-
-
+<!-- Above are the custom library by wawa-->
 <div class="row">
 </div>
 
